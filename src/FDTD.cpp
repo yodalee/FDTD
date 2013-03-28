6de76accf4ba568cb156011e245125036c4c08ee
yodalee <lc85301@gmail.com>
2013-03-28 21:31:03 +0800

    to be merge

--- src/FDTD.cpp
+++ src/FDTD.cpp
@@ -28,20 +28,30 @@ source* FDTD::createsource(string type){
 
 void FDTD::solve(){
 	for (int iterate = 0; iterate < max_iteration; ++iterate) {
-		solveone(V,I,_V,_I); //calculate solution by V,I to _V,_I
-		//copy solution data from _V,_I to V,I
-		for (int copy_idx = 0; copy_idx != gridi_bound; ++copy_idx) {
-			V[copy_idx] = _V[copy_idx];
-			I[copy_idx] = _I[copy_idx];
-		}
-		V[gridi_bound] = _V[gridi_bound];
-		//need a proper way to 
 		cout << time << " ";
-		for (int idx = 0; idx < gridi_bound+1; ++idx) {
-			cout << V[idx] << " ";
+		if (iterate%2 == 0) {
+			solveone(V,I,_V,_I); //calculate solution by V,I to _V,_I
+			for (int idx = 0; idx < gridi_bound+1; ++idx) { cout << _V[idx] << " "; }
+		} else {
+			solveone(_V,_I,V,I); //calculate solution by _V,_I to V,I
+			for (int idx = 0; idx < gridi_bound+1; ++idx) { cout << V[idx] << " "; }
 		}
 		cout << endl;
 		time += delta_t;
+		//solveone(V,I,_V,_I); //calculate solution by V,I to _V,_I
+		//copy solution data from _V,_I to V,I
+		//for (int copy_idx = 0; copy_idx != gridi_bound; ++copy_idx) {
+		//	V[copy_idx] = _V[copy_idx];
+		//	I[copy_idx] = _I[copy_idx];
+		//}
+		//V[gridi_bound] = _V[gridi_bound];
+		//need a proper way to 
+		//cout << time << " ";
+		//for (int idx = 0; idx < gridi_bound+1; ++idx) {
+		//	cout << V[idx] << " ";
+		//}
+		//cout << endl;
+		//time += delta_t;
 	}
 };
 //********************************************
@@ -76,19 +86,26 @@ void FDTD::solveone(const double* V1, const double* I1, double* V2, double* I2){
 // Structure limit:
 //	propagation speed u, time slice delta_t, space slice delta_x	
 //	1. wave length section: lambda > 10*delta_x
-//	2. courant limit: u * delta_t / delta_x <= 1
+//	2. I found that 2*Z/Rs, 2*Z/Rl are also important parameter
+//	3. courant limit: u * delta_t / delta_x <= 1
 //	detemine the delta_t
 //********************************************
-void FDTD::initialStruct(double x, int xsec, double time, int tsec, string type){
+void FDTD::initialStruct(double x, int xsec, double t, int tsec, string type){
 	input = generate_source(type);
 	double nyquist = input->set(1.0, frequency);
 	//set the source
 	double u = 1 / sqrt(capacitance * inductance);
 	double lambda = u/frequency;
+	//determine delta_x, delta_t by some limit
+	//delta_x = lambda/20;
+	//double limit = 0.25*capacitance*delta_x;
+	//delta_t = min(0.5*delta_x/u, limit*Rs, limit*Rl, 1/nyquist);
+	//max_iteration = t/delta_t;
 	delta_x = x/xsec;
-	delta_t = 1/tsec;
-	max_iteration = tsec;
-	//check limit
+	delta_t = 1.0/tsec;
+	cout << t << endl;
+	max_iteration = t / delta_t;
+	////check limit
 	if (lambda < 10*delta_x) { cerr << "x section too large\n"; }
 	if (delta_t >= 0.5*delta_x/u) { cerr << "doesn't fit courant limit\n";}
 	if (delta_t * nyquist > 1) { cerr << "doesn't fit nyquist limit\n";}
