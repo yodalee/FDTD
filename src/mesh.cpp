a6c79d2f118f484e994c67c215675d430a70901a
yodalee <lc85301@gmail.com>
2013-04-09 00:07:31 +0000

    PML mesh

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -6,14 +6,28 @@ double mesh::Dt = 0;
 mesh::mesh(double m, double e){
 	Ex = 0;
 	Ey = 0;
-	Hz = 0;
-	DH = Dt/(Ds*m);
-	CE = Dt/(Ds*e);
+	Hzx = 0;
+	Hzy = 0;
+	setMaterial(m,e);
 }
 
-void mesh::setMaterial(double m, double e, double sigma){
-	DH = Dt/(Ds*m);
-	CE = Dt/(Ds*e);
+void mesh::setMaterial(
+		double m, double e, double sigx, double sigy){
+	double term=0;
+	double sigx_ = sigx * imp0 * imp0;
+	double sigy_ = sigy * imp0 * imp0;
+	term=0.5*(sigy*Dt)/(e);
+	CEx1 = (1-term)/(1+term);
+	CEx2 = (Dt/(e*Ds))/(1+term);
+	term=0.5*(sigx*Dt)/(e);
+	CEy1 = (1-term)/(1+term);
+	CEy2 = (Dt/(e*Ds))/(1+term);
+	term=0.5*(sigx_*Dt)/(m);
+	DHx1 = (1-term)/(1+term);
+	DHx2 = (Dt/(m*Ds))/(1+term);
+	term=0.5*(sigy_*Dt)/(m);
+	DHy1 = (1-term)/(1+term);
+	DHy2 = (Dt/(m*Ds))/(1+term);
 }
 
 void mesh::setstatic(double _Ds, double _Dt){
--- src/mesh.h
+++ src/mesh.h
@@ -14,14 +14,16 @@ typedef struct mesh mesh;
 struct mesh {
 	public:
 		mesh(double m = mu0, double e = eps0);
-		void setMaterial(double m, double e, double sigma=0);
+		void setMaterial(double m, double e, double sigx=0, double sigy=0);
 	public:
 		static void setstatic(double Ds, double Dt);
 		static double Ds;
 		static double Dt;
-		double Ex, Ey, Hz;
-		double DH;
-		double CE;
+		double Ex, Ey, Hzx, Hzy;
+		double CEx1, CEx2;
+		double CEy1, CEy2;
+		double DHx1, DHx2;
+		double DHy1, DHy2;
 };
 
 #endif
